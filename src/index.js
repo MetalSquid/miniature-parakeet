a4f056a48c6029049db11f9b12833cb40b64f744
MetalSquid <bslowinskird@gmail.com>
2023-06-19 12:51:49 -0400

    initial setup

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from '/home/benhain/Documents/Projects/foodapp/rooster/src/components/App.js';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
